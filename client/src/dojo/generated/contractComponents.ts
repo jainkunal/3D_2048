c0aab95a1fbc0022fa9785fb002cbea167224a40
Kunal Jain <kunal@joinwido.com>
2024-06-22 19:35:27 +0530

    update component with my world

--- client/src/dojo/generated/contractComponents.ts
+++ client/src/dojo/generated/contractComponents.ts
@@ -2,44 +2,61 @@
 
 import { defineComponent, Type as RecsType, World } from "@dojoengine/recs";
 
-export type ContractComponents = Awaited<
-    ReturnType<typeof defineContractComponents>
->;
+export type ContractComponents = Awaited<ReturnType<typeof defineContractComponents>>;
 
 export function defineContractComponents(world: World) {
-    return {
-        Moves: (() => {
-            return defineComponent(
-                world,
-                {
-                    player: RecsType.BigInt,
-                    remaining: RecsType.Number,
-                    last_direction: RecsType.Number,
-                },
-                {
-                    metadata: {
-                        name: "Moves",
-                        types: ["contractaddress", "u8", "enum"],
-                        customTypes: ["Direction"],
-                    },
-                }
-            );
-        })(),
-        Position: (() => {
-            return defineComponent(
-                world,
-                {
-                    player: RecsType.BigInt,
-                    vec: { x: RecsType.Number, y: RecsType.Number },
-                },
-                {
-                    metadata: {
-                        name: "Position",
-                        types: ["contractaddress", "u32", "u32"],
-                        customTypes: ["Vec2"],
-                    },
-                }
-            );
-        })(),
-    };
+  return {
+    Tile: (() => {
+      return defineComponent(
+        world,
+        { player: RecsType.BigInt, game_id: RecsType.Number, x: RecsType.Number, y: RecsType.Number, z: RecsType.Number, is_alive: RecsType.Boolean, value: RecsType.Number },
+        {
+          metadata: {
+            name: "Tile",
+            types: ["contractaddress","u32","u32","u32","u32","bool","u32"],
+            customTypes: [],
+          },
+        }
+      );
+    })(),
+    Game: (() => {
+      return defineComponent(
+        world,
+        { player: RecsType.BigInt, game_id: RecsType.Number, game_mode: RecsType.Number, width: RecsType.Number, height: RecsType.Number, tile_count: RecsType.Number, score: RecsType.Number, state: RecsType.Number },
+        {
+          metadata: {
+            name: "Game",
+            types: ["contractaddress","u32","enum","u32","u32","u32","u32","u8"],
+            customTypes: ["GameMode"],
+          },
+        }
+      );
+    })(),
+    Player: (() => {
+      return defineComponent(
+        world,
+        { address: RecsType.BigInt, last_game_id: RecsType.Number, games_won: RecsType.Number, games_lost: RecsType.Number },
+        {
+          metadata: {
+            name: "Player",
+            types: ["contractaddress","u32","u32","u32"],
+            customTypes: [],
+          },
+        }
+      );
+    })(),
+    Moved: (() => {
+      return defineComponent(
+        world,
+        { player: RecsType.BigInt, direction: RecsType.Number },
+        {
+          metadata: {
+            name: "Moved",
+            types: ["contractaddress","enum"],
+            customTypes: ["Direction"],
+          },
+        }
+      );
+    })(),
+  };
 }
