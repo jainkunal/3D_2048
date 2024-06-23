02bb92b57d572bf88666b15fbda29d32dbc2c61a
Kunal Jain <kunal@joinwido.com>
2024-06-23 14:54:38 +0530

    added color to the boxes

--- client/src/gameComponents/TileComponent.tsx
+++ client/src/gameComponents/TileComponent.tsx
@@ -36,9 +36,26 @@ export const TileComponent = (props: any) => {
     const textTexture = useMemo(() => createTextTexture(tile.value), [tile.value]);
 
     const innerMaterials = useMemo(() => {
+        const getColor = (value) => {
+            switch (value) {
+                case 2: return '#eee4da';
+                case 4: return '#ede0c8';
+                case 8: return '#f2b179';
+                case 16: return '#f59563';
+                case 32: return '#f67c5f';
+                case 64: return '#f65e3b';
+                case 128: return '#edcf72';
+                case 256: return '#edcc61';
+                case 512: return '#edc850';
+                case 1024: return '#edc53f';
+                case 2048: return '#edc22e';
+                default: return '#cdc1b4'; // for empty tiles or higher values
+            }
+        };
         return new Array(6).fill(
             new THREE.MeshPhongMaterial({
                 map: textTexture,
+                color: getColor(tile.value),
                 // transparent: true,
                 opacity: 0.5
             })
